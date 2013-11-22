9600fafff8101bc787c2b5a4ab6ae05549cd7ee4
Zhaoren Yu <zhaoren.yu@taodinet.com>
2013-11-22 11:01:26 +0800

    support websocket

--- src/amqp_subscriber.erl
+++ src/amqp_subscriber.erl
@@ -140,9 +140,9 @@ handle_info(#'basic.cancel_ok'{}, State) ->
     {noreply, State};
 
 handle_info({#'basic.deliver'{delivery_tag = _Tag}, {_, _, Message} = _Content}, State) ->
-    {ok, FD} = file:open("trace.log", write),
-    io:format(FD,"> ~ts~n", [Message]),
     io:format("> ~ts~n", [Message]),
+    Msg={bigwig_trace, Message},
+    bigwig_pubsubhub:notify(Msg),
     {noreply, State};
 
 handle_info(_Info, State) ->
